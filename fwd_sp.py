24dd7083ac60114ca8bd664843513fa7daaac2b1
M. Heriyanto <mheriyanto@alumni.itb.ac.id>
2020-01-14 10:56:01 +0700

    add fwd_sp.py

--- fwd_sp.py
+++ fwd_sp.py
@@ -1,3 +1,6 @@
+# Code was created by M. Heriyanto, 2020/01/13
+# https://github.com/ezygeo-ai/machine-learning-and-geophysical-inversion/edit/master/fwd_sp.py
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
@@ -28,7 +31,7 @@ def SPfunc(x, par):
 
 # === TEST FORWARD MODELING
 x0 = 77.07  # m
-alpha = 309.37
+alpha = 309.37 * (np.pi/180)    # deg2rad
 h = 41.81   # m
 K = 94686
 
@@ -41,6 +44,8 @@ get_SPData, get_SPData_noise, noise_from_maxData = SPfunc(measure_loc, par_mod) 
 plt.figure()
 plt.plot(measure_loc, get_SPData, 'b.')
 plt.plot(measure_loc, get_SPData_noise, 'r*')
+plt.xlim([0, 150])
+plt.ylim([-10, 50])
 plt.xlabel('position (m)')
 plt.ylabel('SP data (mV)')
 plt.legend(['ori', 'noise'])
